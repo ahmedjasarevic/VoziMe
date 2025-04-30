d7f36a5399dd18efc898041f40b153708c865a77
ahmedjasarevic <jasarevicahmed999@gmail.com>
2025-04-30 18:07:08 +0200

    Dodana baza

--- VoziMe/MauiProgram.cs
+++ VoziMe/MauiProgram.cs
@@ -4,6 +4,8 @@ using Microsoft.Maui.Controls.Xaml;
 using Microsoft.Maui.Hosting;
 using Microsoft.Maui.Maps;
 using VoziMe.Services;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace VoziMe;
 
@@ -12,6 +14,11 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
+        // Direktno uneseni konekcioni string
+        var connectionString = "User Id=postgres.vfqrsstbgqfwukfgslyo;Password=SanidMuhic123;Server=aws-0-eu-central-1.pooler.supabase.com;Port=5432;Database=postgres";
+
+
         builder
             .UseMauiApp<App>()
             .UseMauiMaps()
@@ -21,11 +28,14 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        // Register services
+        // Registruj servise
         builder.Services.AddSingleton<DatabaseService>();
         builder.Services.AddSingleton<UserService>();
         builder.Services.AddSingleton<DriverService>();
         builder.Services.AddSingleton<LocationService>();
+
+        // Registruj Supabase konekciju sa bazom kao singleton
+        builder.Services.AddSingleton<NpgsqlConnection>(_ => new NpgsqlConnection(connectionString));
 
 #if DEBUG
         builder.Logging.AddDebug();
