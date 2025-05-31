17b71b5b43e54bb44828c3c8e50ae9222867e3fb
Dzenan <124881701+dzenan12345@users.noreply.github.com>
2025-05-31 12:12:07 +0200

    Update User.cs

--- VoziMe/Models/User.cs
+++ VoziMe/Models/User.cs
@@ -2,7 +2,6 @@
 
 namespace VoziMe.Models
 {
-    // Enum za različite vrste korisnika
     public enum UserType
     {
         Customer = 1, // Korisnik
@@ -12,12 +11,12 @@ namespace VoziMe.Models
     // Klasa korisnika
     public class User
     {
-        public int Id { get; set; }                // ID korisnika
-        public string Name { get; set; }            // Ime korisnika
-        public string Email { get; set; }           // Email korisnika
-        public string PasswordHash { get; set; }    // Hashovana lozinka
-        public UserType UserType { get; set; }      // Tip korisnika (Customer/Driver)
-        public string ProfileImage { get; set; }    // Putanja do slike profila
-        public DateTime CreatedAt { get; set; }     // Datum kada je korisnik kreiran
+        public int Id { get; set; }                
+        public string Name { get; set; }           
+        public string Email { get; set; }          
+        public string PasswordHash { get; set; }    
+        public UserType UserType { get; set; }     
+        public string ProfileImage { get; set; }    
+        public DateTime CreatedAt { get; set; }    
     }
 }
